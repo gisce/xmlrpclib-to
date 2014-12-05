120ee1c0cd76fd552b66a09870338dd0e4f2ec67
Eduard Carreras <ecarreras@gisce.net>
2014-12-05 08:49:43 +0100

    Adding tests

--- setup.py
+++ setup.py
@@ -1,10 +1,40 @@
+import sys
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+test_requires = [
+    'pytest',
+    # ATM we need this patch
+    'https://github.com/ecarreras/HTTPretty/archive/simulating_timeout.zip'
+]
 
 
 setup(
     name='xmlrpclib-to',
     version='0.1.1',
     packages=find_packages(),
+    test_requires=test_requires,
+    cmdclass={'test': PyTest},
     url='http://github.com/gisce/xmlrpclib-to',
     license='MIT',
     author='GISCE-TI, S.L.',
